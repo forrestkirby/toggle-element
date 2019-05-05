69a66e58451e80ffe624abf59ce3690bcd39ab5a
forrestkirby <18228336+forrestkirby@users.noreply.github.com>
2019-05-05 16:02:00 +0200

    Update template.php

--- toggle/templates/template.php
+++ toggle/templates/template.php
@@ -7,29 +7,29 @@ $el = $this->el('div', [
 ]);
 
 $uniqid = uniqid('toggle-');
-$ishidden = $props['field_hidden'] ? 'hidden' : '' ;
-$cls = ( $props['field_hidden'] || $props['field_cls'] == '' ) ? 'false' : $props['field_cls'];
+$ishidden = $props['hidden'] ? 'hidden' : '' ;
+$cls = ( $props['hidden'] || $props['cls'] == '' ) ? 'false' : $props['cls'];
 
 ?>
 
 <?= $el($props, $attrs) ?>
 
-	<div class="uk-margin"><?= $props['field_content'] ?></div>
+	<div class="uk-margin"><?= $props['content'] ?></div>
 	<div class="uk-margin">
-		<button class="uk-button <?= $props['field_btn_style'] ?> <?= $props['field_btn_size'] ?>" type="button" uk-toggle="target: #<?= $uniqid ?>; mode: <?= $props['field_mode'] ?>; cls: <?= $cls; ?>; media: <?= $props['field_media'] ?>; animation: <?= $props['field_animation'] ?>; duration: <?= $props['field_duration'] ?>">
+		<button class="uk-button <?= $props['btn_style'] ?> <?= $props['btn_size'] ?>" type="button" uk-toggle="target: #<?= $uniqid ?>; mode: <?= $props['mode'] ?>; cls: <?= $cls; ?>; media: <?= $props['media'] ?>; animation: <?= $props['animation'] ?>; duration: <?= $props['duration'] ?>">
 
-			<?php if ($props['field_icon_align'] == 'left') : ?>
-				<span uk-icon="<?= $props['field_icon'] ?>"></span>
+			<?php if ($props['icon_align'] == 'left') : ?>
+				<span uk-icon="<?= $props['icon'] ?>"></span>
 			<?php endif ?>
 
-			<span class="uk-text-middle"><?= $props['field_btn_label'] ?></span>
+			<span class="uk-text-middle"><?= $props['btn_label'] ?></span>
 
-			<?php if ($props['field_icon_align'] == 'right') : ?>
-				<span uk-icon="<?= $props['field_icon'] ?>"></span>
+			<?php if ($props['icon_align'] == 'right') : ?>
+				<span uk-icon="<?= $props['icon'] ?>"></span>
 			<?php endif ?>
 
 			</button>
 	</div>
-	<div id="<?= $uniqid; ?>" class="uk-margin" <?= $ishidden; ?>><?= $props['field_content2'] ?></div>
+	<div id="<?= $uniqid; ?>" class="uk-margin" <?= $ishidden; ?>><?= $props['content2'] ?></div>
 
 </div>
