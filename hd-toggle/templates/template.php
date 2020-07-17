ae061ef89667627c5c6175af9c1a927af587bd4b
Brian Peat <10532158+brianpeat@users.noreply.github.com>
2020-07-16 21:27:41 -0500

    Update to add toggles
    Updated to make the labels and icons work in open and closed toggles. This file IS A BIG MESS. I am not a coder. Feel free to either use javascript or clean up my horribly inefficient php everywhere. I only wanted to get it working as a test, and it's crazy but it works.

--- hd-toggle/templates/template.php
+++ hd-toggle/templates/template.php
@@ -8,7 +8,7 @@ $el = $this->el('div', [
 
 if ($props['content2'] || $props['target'] == 'false' || $props['target'] == '') {
 	$uniqid = uniqid('toggle-');
-	$target = '#' . $uniqid;
+	$target = '.' . $uniqid;
 } else {
 	$uniqid = '';
 	$target = $props['target'];
@@ -35,19 +35,37 @@ if ($props['toggle_animation_use_advanced']) {
 		<button class="uk-button <?= $props['btn_style'] ?> <?= $props['btn_size'] ?>" type="button" uk-toggle="target: <?= $target ?>; mode: <?= $props['mode'] ?>; cls: <?= $cls; ?>; media: <?= $props['media'] ?>; animation: <?= $animation ?>; duration: <?= $props['duration'] ?>; queued: <?= $queued ?>">
 
 			<?php if ($props['icon_align'] == 'left' && $props['icon']) : ?>
-				<span uk-icon="<?= $props['icon'] ?>"></span>
+				<span uk-icon="<?= $props['icon'] ?>" class="<?php if ($props['icon2']) : ?><?= ($uniqid != '') ? $uniqid : '' ?><?php endif ?>" <?php if ($props['icon2']) : ?>aria-hidden="false"<?php endif ?>></span>
+				<?php if ($props['icon2']) : ?>
+				<span uk-icon="<?= $props['icon2'] ?>" class="<?= ($uniqid != '') ? $uniqid : '' ?>" aria-hidden="true" <?= $ishidden; ?>></span>
+				<?php endif ?>
 			<?php endif ?>
 
-			<span class="uk-text-middle"><?= $props['btn_label'] ?></span>
 
+			<span class="<?php if ($props['btn_label2']) : ?><?= ($uniqid != '') ? $uniqid : '' ?><?php endif ?> uk-text-middle" <?php if ($props['btn_label2']) : ?>aria-hidden="false"<?php endif ?>>
+			<?= $props['btn_label'] ?>
+			</span>
+			
+			<?php if ($props['btn_label2']) : ?>
+			<span class="uk-text-middle <?= ($uniqid != '') ? $uniqid : '' ?>" aria-hidden="true" <?= $ishidden; ?>>
+			<?= $props['btn_label2'] ?>
+			</span>
+			<?php endif ?>
+			
+			
 			<?php if ($props['icon_align'] == 'right' && $props['icon']) : ?>
-				<span uk-icon="<?= $props['icon'] ?>"></span>
+			
+				<span uk-icon="<?= $props['icon'] ?>" class="<?php if ($props['icon2']) : ?><?= ($uniqid != '') ? $uniqid : '' ?><?php endif ?>" <?php if ($props['icon2']) : ?>aria-hidden="false"<?php endif ?>></span>
+				<?php if ($props['icon2']) : ?>
+				<span uk-icon="<?= $props['icon2'] ?>" class="<?= ($uniqid != '') ? $uniqid : '' ?>" aria-hidden="true" <?= $ishidden; ?>></span>
+				<?php endif ?>
+				
 			<?php endif ?>
 
 			</button>
 	</div>
 	<?php if ($props['content2']) : ?>
-		<div <?= ($uniqid != '') ? 'id="' . $uniqid . '"' : '' ?> class="uk-margin" <?= $ishidden; ?>><?= $props['content2'] ?></div>
+		<div class="uk-margin <?= ($uniqid != '') ? $uniqid : '' ?>" <?= $ishidden; ?>><?= $props['content2'] ?></div>
 	<?php endif ?>
 
 </div>
